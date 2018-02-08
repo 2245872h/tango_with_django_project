65874ac3ed1f73c16f033488a4705a2053e12ba0
Emma Hunter <2245872h@student.gla.ac.uk>
2018-02-08 11:55:15 +0000

    end of chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -68,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media'
+
             ],
         },
     },
@@ -130,4 +131,6 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/rango/login/'
 
